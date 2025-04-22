66d7a0421640ce27cda81fa59574e76de14acc56
sendcard <39582415+sendcard@users.noreply.github.com>
2025-04-22 10:08:38 -0600

    Add Traditional Chinese

---
+++ src/lang_zh.h
@@ -0,0 +1,69 @@
+#define FONT(x) x##_tf
+
+//Temperature - Humidity - Forecast
+const String TXT_FORECAST_VALUES = "未來三天預報";
+const String TXT_CONDITIONS = "即時天氣";
+const String TXT_DAYS = "(天)";
+const String TXT_TEMPERATURES = "氣温";
+const String TXT_TEMPERATURE_C = "氣温 (*C)";
+const String TXT_TEMPERATURE_F = "氣温 (*F)";
+const String TXT_HUMIDITY_PERCENT = "空氣濕度 (%)";
+
+// Pressure
+const String TXT_PRESSURE = "氣壓";
+const String TXT_PRESSURE_HPA = "氣壓 (hPa)";
+const String TXT_PRESSURE_IN = "氣壓 (in)";
+const String TXT_PRESSURE_STEADY = "氣壓稳定";
+const String TXT_PRESSURE_RISING = "氣壓上升";
+const String TXT_PRESSURE_FALLING = "氣壓下降";
+
+//RainFall / SnowFall
+const String TXT_RAINFALL_MM = "降雨量 (mm)";
+const String TXT_RAINFALL_IN = "降雨量 (in)";
+const String TXT_SNOWFALL_MM = "降雪量 (mm)";
+const String TXT_SNOWFALL_IN = "降雪量 (in)";
+const String TXT_PRECIPITATION_SOON = "Prec.";
+
+//Sun
+const String TXT_SUNRISE = "日出時間";
+const String TXT_SUNSET = "日落時間";
+
+//Moon
+const String TXT_MOON_NEW = "新月";
+const String TXT_MOON_WAXING_CRESCENT = "蛾眉月";
+const String TXT_MOON_FIRST_QUARTER = "上弦月";
+const String TXT_MOON_WAXING_GIBBOUS = "盈月";
+const String TXT_MOON_FULL = "滿月";
+const String TXT_MOON_WANING_GIBBOUS = "虧月";
+const String TXT_MOON_THIRD_QUARTER = "下弦月";
+const String TXT_MOON_WANING_CRESCENT = "下蛾眉月";
+
+//Power / WiFi
+const String TXT_POWER = "電量";
+const String TXT_WIFI = "WiFi";
+const char* TXT_UPDATED = "更新时時:";
+
+//Wind
+const String TXT_WIND_SPEED_DIRECTION = "風速/風向";
+const String TXT_N = "北";
+const String TXT_NNE = "東北偏北";
+const String TXT_NE = "東北";
+const String TXT_ENE = "東北偏東";
+const String TXT_E = "東";
+const String TXT_ESE = "東南偏東";
+const String TXT_SE = "東南";
+const String TXT_SSE = "東南偏南";
+const String TXT_S = "南";
+const String TXT_SSW = "西南偏南";
+const String TXT_SW = "西南";
+const String TXT_WSW = "西南偏西";
+const String TXT_W = "西";
+const String TXT_WNW = "西北偏西";
+const String TXT_NW = "西北";
+const String TXT_NNW = "西北偏北";
+
+//Day of the week
+const char* weekday_D[] = { "週日", "週一", "週二", "週三", "週四", "週五", "週六" };
+
+//Month
+const char* month_M[] = { "一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月" };
